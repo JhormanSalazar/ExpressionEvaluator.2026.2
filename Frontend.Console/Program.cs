185953baa710bb57d3561e0d94c389d4b0ac621d
Jhorman Salazar <salazarjhorman181@gmail.com>
2026-09-21 16:58:28 -0500

    Add multi-digit and decimal test cases to console

--- Frontend.Console/Program.cs
+++ Frontend.Console/Program.cs
@@ -11,3 +11,30 @@ Console.WriteLine($"Infix = {infix3}, Result = {ExpressionEvaluator.Evalute(infi
 
 var infix4 = "144^(1/2)";
 Console.WriteLine($"Infix = {infix4}, Result = {ExpressionEvaluator.Evalute(infix4):N5}"); // 12
+
+var infix5 = "12+34";
+Console.WriteLine($"Infix = {infix5}, Result = {ExpressionEvaluator.Evalute(infix5):N5}"); // 46
+
+var infix6 = "100/10/2";
+Console.WriteLine($"Infix = {infix6}, Result = {ExpressionEvaluator.Evalute(infix6):N5}"); // 5
+
+var infix7 = "10-4-3";
+Console.WriteLine($"Infix = {infix7}, Result = {ExpressionEvaluator.Evalute(infix7):N5}"); // 3
+
+var infix8 = "1-2*3+4";
+Console.WriteLine($"Infix = {infix8}, Result = {ExpressionEvaluator.Evalute(infix8):N5}"); // -1
+
+var infix9 = "2^3^2";
+Console.WriteLine($"Infix = {infix9}, Result = {ExpressionEvaluator.Evalute(infix9):N5}"); // 512
+
+var infix10 = "0.5*4";
+Console.WriteLine($"Infix = {infix10}, Result = {ExpressionEvaluator.Evalute(infix10):N5}"); // 2
+
+var infix11 = "3.75+1.25";
+Console.WriteLine($"Infix = {infix11}, Result = {ExpressionEvaluator.Evalute(infix11):N5}"); // 5
+
+var infix12 = "25";
+Console.WriteLine($"Infix = {infix12}, Result = {ExpressionEvaluator.Evalute(infix12):N5}"); // 25
+
+var infix13 = "(3.1416+70)/(300^(1/3.2))";
+Console.WriteLine($"Infix = {infix13}, Result = {ExpressionEvaluator.Evalute(infix13):N5}"); // 12.30450
